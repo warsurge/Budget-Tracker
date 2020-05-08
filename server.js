b7b80c610f07a3b2b5c60b57aeab6acd7abaf856
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-08 17:32:05 -0500

    added heroku

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI,"mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
