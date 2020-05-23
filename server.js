f3b1a7801a95f718c39df4b89e9536fdc2585735
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-23 13:44:59 -0500

    put old code back in

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI,"mongodb://localhost/budget", {
+
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
